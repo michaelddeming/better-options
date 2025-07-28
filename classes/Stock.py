c36aafe3af5ce1ce9acc98998e3cda2db4b07901
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-07-27 21:41:20 -0700

    Add Stock class to get ticker and options info.

---
+++ classes/Stock.py
@@ -0,0 +1,31 @@
+import yfinance as yf
+import csv
+import pandas as pd
+class Stock:
+
+    def __init__(self, ticker: str):
+
+        self.ticker = ticker.upper()
+
+        self.data = yf.Ticker(self.ticker)
+    
+    def get_option_chain(self, expiration_index: int=0, export_chain:bool=False):
+
+        options = self.data.options
+        if not options:
+            raise ValueError(f"OptionError: No options found for {self.ticker}.")
+        expiration_date = options[expiration_index]
+        chain = self.data.option_chain(expiration_date)
+        if export_chain:
+            with open(f"{self.ticker.lower()}_call_option_chain.csv", "w", newline="") as file:
+                chain.calls.to_csv(file)
+            with open(f"{self.ticker.lower()}_put_option_chain.csv", "w", newline="") as file:
+                chain.puts.to_csv(file)
+        return chain.calls, chain.puts
+
+
+msft = Stock(ticker="MSFT")
+msft_calls, msft_puts = msft.get_option_chain(expiration_index=0, export_chain=True)
+print(msft_calls)
+
+
