68870d63c55bdd11e0fabf310301a5fa32ea1af9
michaelddeming <169874829+michaelddeming@users.noreply.github.com>
2025-08-05 21:25:19 -0700

    Add limiter to get_options_chain().

--- testing.py
+++ testing.py
@@ -2,15 +2,14 @@ from classes.Stock import Stock
 from classes.Option import Option
 
 
-msft = Stock(ticker="MSFT")
-expiration_date, msft_calls, msft_puts = msft.get_option_chain(expiration_index=0, export_chain=True)
+msft = Stock(ticker="aapl")
+expiration_date, msft_calls, msft_puts = msft.get_option_chain(
+    expiration_index=10, export_chain=True, limit=5
+)
 
-prices = []
 for option in msft_calls:
-    bs = round(msft.black_scholes_model(option, .0442), 2)
+    bs = round(msft.black_scholes_model(option, 0.0442), 2)
     curr = round(((option.bid + option.ask) / 2), 2)
-    change = round(((bs - curr) / curr) * 100, 2) 
-    prices.append((option.strike,curr, bs, change))
-
-print(prices)
+    change = round(((curr - bs) / bs) * 100, 2)
+    print(option.strike, curr, bs, change)
 
